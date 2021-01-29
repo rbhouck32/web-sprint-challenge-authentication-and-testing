1c9a0e87df329f10bc5c3ad6da12d04edb4be12c
Rob <rbhouck32@gmail.com>
2021-01-29 11:52:15 -0500

    commit contains the completion of the restricted middleware functionality

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,14 +1,18 @@
+const jwt = require("jsonwebtoken");
+const { jwtSecret } = require("../../config/secrets.js");
+
 module.exports = (req, res, next) => {
-  next();
-  /*
-    IMPLEMENT
-
-    1- On valid token in the Authorization header, call next.
-
-    2- On missing token in the Authorization header,
-      the response body should include a string exactly as follows: "token required".
-
-    3- On invalid or expired token in the Authorization header,
-      the response body should include a string exactly as follows: "token invalid".
-  */
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, jwtSecret, (err, decoded) => {
+      if (err) {
+        res.status(401).json("token required");
+      } else {
+        req.decodedJwt = decoded;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json("token invalid");
+  }
 };
