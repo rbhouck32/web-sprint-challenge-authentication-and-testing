47144c9dc0ef4944da6e1651360c3e33bd1b1f93
Rob <rbhouck32@gmail.com>
2021-01-29 12:30:29 -0500

    cleans up project comments

--- api/server.js
+++ api/server.js
@@ -16,6 +16,6 @@ server.use(express.json());
 
 server.use("/api/auth", authRouter);
 server.use("/api/users", usersRouter);
-server.use("/api/jokes", restrict, jokesRouter); // only logged-in users should have access!
+server.use("/api/jokes", restrict, jokesRouter);
 
 module.exports = server;
